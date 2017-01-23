94ab071ba52bd9c0d4c53c4fd91e0f5409f842c1
Vishal Patel <vishal@comitydesigns.com>
2017-01-23 16:46:39 -0500

    Updated

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.set('view engine', 'ejs');
 app.get('/', function(req, res){
 	console.log('RedirectToHome call');
 	
-	oauth.redirectToHome(res, app);
+	oauth.redirectToHome(req, res, app);
 	//oauth.redirectAuthURI(res);
 });
 
