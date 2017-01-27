0f8caf9b2e88703d59ee6cc5f4c2be636c54632b
Amit <amit@comitydesigns.com>
2017-01-27 16:23:11 +0530

    Updated code to use REDIS

--- routes/home.js
+++ routes/home.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var oauth = require('./lib/oAuth/oauth');
+
+oauth.redisClient.get((req.ordid + req.userid), function(err, reply){
+	req.app.locals.oauthtoken = reply;
+});
 
 router.get('/', function(req, res) {
 	    res.render('pages/home', {
-	        oauthtoken: req.app.locals.oauthtoken,
+	    	oauthtoken: req.app.locals.oauthtoken,
 	        ouathLightningURL: req.app.locals.lightningEndPointURI
     });
 });
