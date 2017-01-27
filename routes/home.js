75ef1219ce64334c3cd2ceed09605fffbaac5b9f
Amit <amit@comitydesigns.com>
2017-01-27 17:11:47 +0530

    Removed redis code.

--- routes/home.js
+++ routes/home.js
@@ -1,18 +1,18 @@
 var express = require('express');
 var router = express.Router();
 //var oauth = require('./lib/oAuth/oauth');
-var redis = require('redis');
+/*var redis = require('redis');
 var port = process.env.PORT || 3000;
 var redisClient = redis.createClient(port, 'localhost');
 redisClient.on('connect', function() {
     console.log('Redis connected home ejs');
-});
+});*/
 
 router.get('/', function(req, res) {
-		redisClient.get((req.orgid + req.userid), function(err, reply){
+		/*redisClient.get((req.orgid + req.userid), function(err, reply){
 			console.log('reply --> '+reply);
 			req.app.locals.oauthtoken = reply;
-		});
+		});*/
 	    res.render('pages/home', {
 	    	oauthtoken: req.app.locals.oauthtoken,
 	        ouathLightningURL: req.app.locals.lightningEndPointURI
