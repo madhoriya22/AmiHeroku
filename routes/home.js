4a37597bc7e1e56b3f20e65daee75f8b399c1388
Amit <amit@comitydesigns.com>
2017-01-31 19:59:44 +0530

    updated console logs

--- routes/home.js
+++ routes/home.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res) {
-	console.log('access token from redis '+req.session.oauthtoken);
-	console.log('url from redis '+req.session.sfdcUrl);
+	console.log('access token from redis '+req.session.accesstoken);
+	console.log('url from redis '+req.session.sfdcurl);
 	    res.render('pages/home', {
 	    	accesstoken: req.session.accesstoken,
 	    	sfdcurl: req.session.sfdcurl
