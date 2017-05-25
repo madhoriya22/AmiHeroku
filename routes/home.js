821b8631933178f71740caf0bee23788e5d43e97
Dheeraj <Dheerajs@comitydesigns.com>
2017-05-25 16:17:29 +0530

    dereference cripto

--- routes/home.js
+++ routes/home.js
@@ -3,12 +3,11 @@ var router = express.Router();
 var crypto = require('crypto'),
 algorithm = 'aes-256-cfb',
 password = 's6b3DheV';
-require('../../lib/mode-cfb.min.js');
 router.get('/', function(req, res) {
 	console.log('access token from redis '+req.session.accesstoken);
 	console.log('url from redis '+req.session.sfdcurl);
 	    res.render('pages/home', {
-	    	accesstoken: encrypt(req.session.accesstoken),
+	    	accesstoken: req.session.accesstoken,
 	    	sfdcurl: req.session.sfdcurl,
 	    	orgid: req.session.orgid,
 	    	namespace: process.env['SF_NAMESPACE'],
