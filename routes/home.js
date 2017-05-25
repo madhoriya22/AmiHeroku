a1f44d622eecd5f39b881cebba54690469fb4036
Dheeraj <Dheerajs@comitydesigns.com>
2017-05-25 12:18:59 +0530

    Home.js update

--- routes/home.js
+++ routes/home.js
@@ -1,16 +1,28 @@
 var express = require('express');
 var router = express.Router();
+var crypto = require('crypto'),
+algorithm = 'aes-256-ctr',
+password = 's6b3DheV';
 
 router.get('/', function(req, res) {
 	console.log('access token from redis '+req.session.accesstoken);
 	console.log('url from redis '+req.session.sfdcurl);
 	    res.render('pages/home', {
-	    	accesstoken: req.session.accesstoken,
+	    	accesstoken: encrypt(req.session.accesstoken),
 	    	sfdcurl: req.session.sfdcurl,
 	    	orgid: req.session.orgid,
 	    	namespace: process.env['SF_NAMESPACE'],
 	    	idletime: process.env['SESSION_IDLE_TIME']
     });
 });
+
+function encrypt(text){
+	   var cipher = crypto.createCipher(algorithm,password)
+	   var crypted = cipher.update(text,'utf8','hex')
+	   crypted += cipher.final('hex');
+	   console.log("Encript code -->> "+crypted);
+	   return crypted;
+	}
+
 
 module.exports = router;
