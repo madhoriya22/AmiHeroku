27eb0f9d67e3a3b461e0394b6df0e98f7d0b1fd8
Dheeraj <Dheerajs@comitydesigns.com>
2017-05-25 12:49:38 +0530

    Update crypto

--- routes/home.js
+++ routes/home.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var crypto = require('crypto'),
-algorithm = 'aes-256-ctr',
+algorithm = 'aes-256-cbc',
 password = 's6b3DheV';
 
 router.get('/', function(req, res) {
