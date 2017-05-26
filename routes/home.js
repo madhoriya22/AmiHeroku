e0a14fbf93d0a249ed49d4ecf71227b358ae0714
Dheeraj <Dheerajs@comitydesigns.com>
2017-05-26 15:26:14 +0530

    Revert back crypto

--- routes/home.js
+++ routes/home.js
@@ -1,17 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
-var cryptoCtr = require('crypto-aes-ctr');
 
-var crypto = require('crypto'),
-algorithm = 'aes-256-ctr',
-password = 'd6F3Efeq';
 
 router.get('/', function(req, res) {
 	console.log('access token from redis '+req.session.accesstoken);
 	console.log('url from redis '+req.session.sfdcurl);
 	    res.render('pages/home', {
-	    	accesstoken: encrypt(req.session.accesstoken),
+	    	accesstoken: req.session.accesstoken,
 	    	sfdcurl: req.session.sfdcurl,
 	    	orgid: req.session.orgid,
 	    	namespace: process.env['SF_NAMESPACE'],
@@ -19,13 +15,5 @@ router.get('/', function(req, res) {
     });
 });
 
-
-
-function encrypt(text){
-	var cipher = crypto.createCipher(algorithm,password)
-	var crypted = cipher.update(text,'utf8','hex')
-	crypted += cipher.final('hex');
-	return crypted;
-}
 
 module.exports = router;
