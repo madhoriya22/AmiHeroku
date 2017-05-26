1490f7dea9db0e1d9b1ed97beb3ca55677a40591
Dheeraj <Dheerajs@comitydesigns.com>
2017-05-26 15:01:19 +0530

    Update cTR algo

--- routes/home.js
+++ routes/home.js
@@ -2,12 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 
+var crypto = require('crypto'),require('crypto-aes-ctr'),
+algorithm = 'aes-256-ctr',
+password = 'd6F3Efeq';
 
 router.get('/', function(req, res) {
 	console.log('access token from redis '+req.session.accesstoken);
 	console.log('url from redis '+req.session.sfdcurl);
 	    res.render('pages/home', {
-	    	accesstoken: req.session.accesstoken,
+	    	accesstoken: encrypt(req.session.accesstoken),
 	    	sfdcurl: req.session.sfdcurl,
 	    	orgid: req.session.orgid,
 	    	namespace: process.env['SF_NAMESPACE'],
@@ -17,5 +20,11 @@ router.get('/', function(req, res) {
 
 
 
+function encrypt(text){
+	var cipher = crypto.createCipher(algorithm,password)
+	var crypted = cipher.update(text,'utf8','hex')
+	crypted += cipher.final('hex');
+	return crypted;
+}
 
 module.exports = router;
