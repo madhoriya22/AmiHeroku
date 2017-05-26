c73f065d43853e55ed598edb52cab29bf5626255
Dheeraj <Dheerajs@comitydesigns.com>
2017-05-26 14:42:19 +0530

    Package update

--- routes/home.js
+++ routes/home.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var router = express.Router();
-var cryptoMode = require('./lib/oAuth/oauth');
+
 
 
 router.get('/', function(req, res) {
 	console.log('access token from redis '+req.session.accesstoken);
 	console.log('url from redis '+req.session.sfdcurl);
 	    res.render('pages/home', {
-	    	accesstoken: cryptoMode.encrypt(req.session.accesstoken),
+	    	accesstoken: req.session.accesstoken,
 	    	sfdcurl: req.session.sfdcurl,
 	    	orgid: req.session.orgid,
 	    	namespace: process.env['SF_NAMESPACE'],
