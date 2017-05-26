e249541c3496c1423e9cbc611a0cc5a5171feac9
Dheeraj <Dheerajs@comitydesigns.com>
2017-05-26 17:32:52 +0530

    update code

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(sessions.createSession());
 // Require Routes js
 var routesHome = require('./routes/home');
 //message to cipher 
-var message = 'Test';
+var message = 'Test method crypto';
 
 // Serve static files
 app.use(express.static(__dirname + '/public'));
