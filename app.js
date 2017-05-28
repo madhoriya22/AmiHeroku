b2653c0d65dcae83e0720ae1d61ba613d16bdd95
Dheeraj <Dheerajs@comitydesigns.com>
2017-05-28 20:02:39 +0530

    test console

--- app.js
+++ app.js
@@ -130,7 +130,8 @@ var JsonFormatter = {
 //browser request serialized cipherParams object in path /crypto/encrypted, with JSONP support 
 app.get('/crypto/encrypted', function(request, response) {
 	oauth.getAccessTokenInRes(request, response, app);
-	console.log("OAuth ==>> "+request.app.locals.oauthtoken);
+	console.log("============================= Hell Yeah =========");
+	console.log(request);
     //JSONP allow cross domain AJAX 
     response.jsonp({
         encrypted : encrypted_json_str
