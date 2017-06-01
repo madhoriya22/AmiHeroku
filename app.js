2d4fc9bee46a00c8b075bd074ef2a6a0ba3b58f8
Dheeraj <Dheerajs@comitydesigns.com>
2017-06-01 12:00:58 +0530

    remove JSONFormatter

--- app.js
+++ app.js
@@ -70,52 +70,11 @@ var encrypted;
  
 // convert CipherParams object to json string for transmission 
 var encrypted_json_str;
-
+ 
 
 //browser request serialized cipherParams object in path /crypto/encrypted, with JSONP support 
 app.get('/crypto/encrypted', function(request, response) {
-	//create custom json serialization format 
-	var JsonFormatter = {
-	    stringify: function (cipherParams) {
-	        // create json object with ciphertext 
-	        var jsonObj = {
-	            ct: cipherParams.ciphertext.toString(CryptoJS.enc.Base64)
-	        };
-	        
-	        // optionally add iv and salt 
-	        if (cipherParams.iv) {
-	            jsonObj.iv = cipherParams.iv.toString();
-	        }
-	        
-	        if (cipherParams.salt) {
-	            jsonObj.s = cipherParams.salt.toString();
-	        }
-	 
-	        // stringify json object 
-	        return JSON.stringify(jsonObj)
-	    },
-	 
-	    parse: function (jsonStr) {
-	        // parse json string 
-	        var jsonObj = JSON.parse(jsonStr);
-	        
-	        // extract ciphertext from json object, and create cipher params object 
-	        var cipherParams = CryptoJS.lib.CipherParams.create({
-	            ciphertext: CryptoJS.enc.Base64.parse(jsonObj.ct)
-	        });
-	        
-	        // optionally extract iv and salt 
-	        if (jsonObj.iv) {
-	            cipherParams.iv = CryptoJS.enc.Hex.parse(jsonObj.iv);
-	        }
-	            
-	        if (jsonObj.s) {
-	            cipherParams.salt = CryptoJS.enc.Hex.parse(jsonObj.s);
-	        }
-	        
-	        return cipherParams;
-	    }
-	};
+	
 	//oauth.getAccessTokenInRes(request, response, app);
 	encrypted = CryptoJS.AES.encrypt(request.session.accesstoken, r_pass_base64, { format: JsonFormatter });
 	encrypted_json_str = encrypted.toString();
