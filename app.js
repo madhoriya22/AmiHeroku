7c7f3571df62722eecf285f62cbe9c5d0d9deab2
Vishal Patel <vishal@comitydesigns.com>
2017-06-02 22:45:04 -0400

    updated

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 "use strict";
-//var newrelic = require('newrelic');
+var newrelic = require('newrelic');
 var express = require('express');
 var oauth = require('./lib/oAuth/oauth');
 var port = process.env.PORT || 3000;
