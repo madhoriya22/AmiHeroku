aca206c828ec3d3822c217fbb956de527a6369e4
madhoriya22 <madhoriya22@gmail.com>
2017-08-22 18:27:34 -0400

    Update welcome.js

--- routes/welcome.js
+++ routes/welcome.js
@@ -4,7 +4,10 @@ console.log('Before going into the welcome page');
 router.get('/welcome', function(req, res) {
 	
 	console.log('About to show welcome page');
-	res.render('pages/welcome');
+	res.render('pages/welcome', {
+	    	orgId: req.query.orgId,
+	    	renId: req.query.renId
+    	});
 });
 
 module.exports = router;
