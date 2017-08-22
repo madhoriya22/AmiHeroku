e6add0e86c252cf48947af92a2f2b703dc8f2b2e
madhoriya22 <madhoriya22@gmail.com>
2017-08-22 15:58:04 -0400

    Update welcome.js

--- routes/welcome.js
+++ routes/welcome.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-
+console.log('Before going into the welcome page');
 router.get('/welcome', function(req, res) {
 	res.render('pages/welcome');
 });
