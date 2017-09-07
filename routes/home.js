f13632548406d4e37c75c7e3e203d20bf435b7f0
Amit <amit@comitydesigns.com>
2017-09-07 23:13:23 +0530

    Test Fix for cookie safari

--- routes/home.js
+++ routes/home.js
@@ -1,10 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res) {
+/*router.get('/', function(req, res) {
 	console.log('access token from redis '+req.session.accesstoken);
 	console.log('url from redis '+req.session.sfdcurl);
 	res.render('pages/home', {
+		str: req.session.accesstoken,
+		sfdcurl: req.session.sfdcurl,
+		orgid: req.session.orgid,
+		namespace: process.env['SF_NAMESPACE'],
+		idletime: process.env['SESSION_IDLE_TIME']
+	});
+});*/
+
+router.get('/', function(req, res) {
+	console.log('access token from redis '+req.session.accesstoken);
+	console.log('url from redis '+req.session.sfdcurl);
+	res.render('pages/fix', {
 		str: req.session.accesstoken,
 		sfdcurl: req.session.sfdcurl,
 		orgid: req.session.orgid,
