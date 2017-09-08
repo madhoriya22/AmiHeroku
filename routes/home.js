7f1dd05368886ddfd67b18b4092b8920a2a8643b
madhoriya22 <madhoriya22@gmail.com>
2017-09-08 13:18:29 -0400

    Update home.js

--- routes/home.js
+++ routes/home.js
@@ -1,22 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-/*router.get('/', function(req, res) {
-	console.log('access token from redis '+req.session.accesstoken);
-	console.log('url from redis '+req.session.sfdcurl);
-	res.render('pages/home', {
-		str: req.session.accesstoken,
-		sfdcurl: req.session.sfdcurl,
-		orgid: req.session.orgid,
-		namespace: process.env['SF_NAMESPACE'],
-		idletime: process.env['SESSION_IDLE_TIME']
-	});
-});*/
-
 router.get('/', function(req, res) {
 	console.log('access token from redis '+req.session.accesstoken);
 	console.log('url from redis '+req.session.sfdcurl);
-	res.render('pages/fix', {
+	res.render('pages/home', {
 		str: req.session.accesstoken,
 		sfdcurl: req.session.sfdcurl,
 		orgid: req.session.orgid,
