741e894fbb0d31bf7556902711b57ac02d028847
Dheeraj <Dheerajs@comitydesigns.com>
2017-09-15 14:26:46 +0530

    US2728..

--- routes/welcome.js
+++ routes/welcome.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res) {
+	console.log('Rounter welcome');
 	res.render('pages/welcome', {
 		orgid: req.session.orgid,
 		communityUrl:req.session.communityUrl
