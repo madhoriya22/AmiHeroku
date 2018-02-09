ab7aa725e3e544c0976c179576abf8077824a753
madhoriya22 <madhoriya22@gmail.com>
2018-02-09 09:07:47 -0500

    Update home.js
    Updated

--- routes/home.js
+++ routes/home.js
@@ -4,6 +4,11 @@ var router = express.Router();
 router.get('/', function(req, res) {
 	console.log('access token from redis '+req.session.accesstoken);
 	console.log('url from redis '+req.session.sfdcurl);
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, X-Response-Time, X-PINGOTHER, X-CSRF-Token,Authorization,X-Authorization'); 
+	res.setHeader('Access-Control-Allow-Methods', '*');
+	res.setHeader('Access-Control-Expose-Headers', 'X-Api-Version, X-Request-Id, X-Response-Time');
+	res.setHeader('Access-Control-Max-Age', '1000');
 	res.render('pages/home', {
 		str: req.session.accesstoken,
 		sfdcurl: req.session.sfdcurl,
