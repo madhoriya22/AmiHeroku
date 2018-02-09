f56050234d4ca4f37aabe5432dad73785c8bc627
madhoriya22 <madhoriya22@gmail.com>
2018-02-09 13:14:32 -0500

    Update app.js
    Updated to include CORS

--- app.js
+++ app.js
@@ -18,13 +18,24 @@ app.use(express.static(__dirname + '/public'));
 app.use('/home', routesHome);
 
 app.set('view engine', 'ejs');
+
+/*Allow CORS*/
+app.all('*', function(req, res, next) {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, X-Response-Time, X-PINGOTHER, X-CSRF-Token,Authorization,X-Authorization'); 
+	res.setHeader('Access-Control-Allow-Methods', '*');
+	res.setHeader('Access-Control-Expose-Headers', 'X-Api-Version, X-Request-Id, X-Response-Time');
+	res.setHeader('Access-Control-Max-Age', '1000');
+	  
+	next();
+});
 	
-app.get('/', function(req, res){
+app.get('/', function(req, res, next){
 	oauth.getCommunityURL(req,res);
 	//oauth.redirectToHome(req, res, app);
 });
 
-app.get('/authenticate', function(req, res){
+app.get('/authenticate', function(req, res, next){
 	oauth.redirectToHome(req, res, app);
 });
 
